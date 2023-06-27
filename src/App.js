8670537b0db240f632f1d4f881937bb1c7cdd8e3
Priyal lohar <priyallohar27@gmail.com>
2023-06-27 10:15:08 +0530

    added header in app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Components/Layout/Header'
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <Header>
+        
+      </Header>
+    </React.Fragment>
+  )
+}
+
+export default App
